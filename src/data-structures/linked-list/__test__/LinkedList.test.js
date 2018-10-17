724634d36df68d0c5b7c70fb49918236ce2336d5
Chuck <tengchenc@gmail.com>
2018-10-17 23:31:33 +0800

    test: linkedList test

--- src/data-structures/linked-list/__test__/LinkedList.test.js
+++ src/data-structures/linked-list/__test__/LinkedList.test.js
@@ -5,4 +5,31 @@ describe('LikedList', () => {
     const linkedList = new LinkedList()
     expect(linkedList.toString()).toBe('')
   })
+
+  it('should append node to linked list', () => {
+    const linkedList = new LinkedList()
+
+    expect(linkedList.head).toBeNull()
+    expect(linkedList.tail).toBeNull()
+
+    linkedList.append(1)
+    linkedList.append(2)
+
+    expect(linkedList.toString()).toBe('1,2')
+    expect(linkedList.tail.next).toBeNull()
+  })
+
+  it('should preppend node to linked list', () => {
+    const linkedList = new LinkedList()
+
+    linkedList.prepend(2)
+
+    expect(linkedList.head.toString()).toBe('2')
+    expect(linkedList.tail.toString()).toBe('2')
+
+    linkedList.append(1)
+    linkedList.prepend(3)
+
+    expect(linkedList.toString()).toBe('3,2,1')
+  })
 })
