d34a30a7ff87a363e6fe2a10e66287063d3ddb02
Chuck <tengchenc@gmail.com>
2018-11-11 13:53:56 +0800

    add queue

---
+++ src/data-structures/queque/Queue.js
@@ -0,0 +1,24 @@
+import LinkedList from '../linked-list/LinkedList'
+
+export default class Queue {
+  constructor () {
+    this.linkedList = new LinkedList()
+  }
+  peek () {
+    if (!this.linkedList.head) {
+      return null
+    }
+
+    return this.linkedList.head.value
+  }
+  enqueue (value) {
+    this.linkedList.append(value)
+  }
+  dequeue () {
+    const removeHead = this.linkedList.deleteHead
+    return removeHead ? removeHead.value : null
+  }
+  toString (callback) {
+    return this.linkedList.toString(callback)
+  }
+}
